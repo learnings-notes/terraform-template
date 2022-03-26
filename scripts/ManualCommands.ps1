fec40aff09e0b82f38b20a1b2feec440ffcbcd4b
jamalikake <jamalirkp@gmail.com>
2022-03-27 00:22:39 +0530

    Update ManualCommands.ps1

--- scripts/ManualCommands.ps1
+++ scripts/ManualCommands.ps1
@@ -10,7 +10,7 @@ az storage account create -n ghtfstatefilesa -g tfstatefile-rg -l eastus2 --sku 
 az storage container create -n rgtfstatecontainer --connection-string "DefaultEndpointsProtocol=https;AccountName=ghtfstatefilesa;AccountKey=4xXUxgRqQjT96OMpnb10JBfZb74U2P7dN4Y8TAew1dGti7g7Ylf9oIwIBV38OojTcImGIx+ziJHqIwymecleNw==;EndpointSuffix=core.windows.net"
 
 
-#Terraform service Principles
+#Terraform service Principles.
 az ad sp create-for-rbac --name azure1-terraform-sp
 #az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/8a1b32ad-e119-4091-8703-e124f11fc6f2"
 #az login --service-principal -u "39542937-6641-4bd9-99e4-e7fca4fbdc5a" -p "Mx_w_iRL28E3JicTAPPx7KpkpdUszGorSH" --tenant "4fd1514d-311a-4354-8623-4d895779124e"
